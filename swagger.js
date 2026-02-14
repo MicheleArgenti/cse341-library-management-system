828d7ae8cec973d2725b9af56e0d5184c5579ca2
raghork <michele.argenti000@gmail.com>
2026-02-14 19:41:54 +0100

    W05 Final Project Part 1: First Two Collections with Crud Operations and API Documentation

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API documentation for the CSE341 Node.js Final Project -> Library Management System',
   },
   host: 'localhost:3000',
-  schemes: ['http', 'https'],
+  schemes: ['https', 'http'],
 };
 
 const outputFile = './swagger.json';
